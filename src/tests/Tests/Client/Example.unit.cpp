b8242abcc46c9c94ac6ca05cfc80bb4cc4584132
Martin Helmut Fieber <info@martin-fieber.de>
2020-10-10 16:18:57 +0200

    Add base test structure for Core

--- src/tests/Tests/Client/Example.unit.cpp
+++
@@ -1,7 +0,0 @@
-#include <doctest/doctest.h>
-
-TEST_SUITE("Example") {
-  TEST_CASE("show test case") {
-    CHECK(1 + 2 == 3);
-  }
-}
---
+++ src/tests/Tests/Core/ConfigFile.unit.cpp
@@ -0,0 +1,7 @@
+#include <doctest/doctest.h>
+
+TEST_SUITE("ConfigFile") {
+  TEST_CASE("Resolve configuration file") {
+    CHECK(1 + 2 == 3);
+  }
+}
