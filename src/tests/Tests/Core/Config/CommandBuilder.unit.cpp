e6a531b3b0a569e2fa91a8b610953e30094e4e34
Martin Helmut Fieber <info@martin-fieber.de>
2020-11-20 21:44:00 +0100

    Add tests for CommandBuilder description and example

--- src/tests/Tests/Core/Config/CommandBuilder.unit.cpp
+++ src/tests/Tests/Core/Config/CommandBuilder.unit.cpp
@@ -79,4 +79,68 @@ TEST_SUITE("CommandBuilder") {
       CHECK(result->Script[1] == "second line");
     }
   }
+
+  TEST_CASE("CommandBuilder::AddDescription") {
+    SUBCASE("Does nothing if description is not set") {
+      auto [file, data] = CreateTOMLMock("test", R"(key = "value")");
+
+      Litr::CommandBuilder builder{file, data, "test"};
+      builder.AddDescription();
+
+      CHECK(builder.GetErrors().size() == 0);
+      CHECK(builder.GetResult()->Description.empty());
+    }
+
+    SUBCASE("Emits and error if description is not a string") {
+      auto [file, data] = CreateTOMLMock("test", R"(description = 42)");
+
+      Litr::CommandBuilder builder{file, data, "test"};
+      builder.AddDescription();
+
+      CHECK(builder.GetErrors().size() == 1);
+      CHECK(builder.GetErrors()[0].Message == R"(The "description" can can only be a string.)");
+    }
+
+    SUBCASE("Extracts the description from toml data") {
+      auto [file, data] = CreateTOMLMock("test", R"(description = "Text")");
+
+      Litr::CommandBuilder builder{file, data, "test"};
+      builder.AddDescription();
+
+      CHECK(builder.GetErrors().size() == 0);
+      CHECK(builder.GetResult()->Description == "Text");
+    }
+  }
+
+  TEST_CASE("CommandBuilder::AddExample") {
+    SUBCASE("Does nothing if example is not set") {
+      auto [file, data] = CreateTOMLMock("test", R"(key = "value")");
+
+      Litr::CommandBuilder builder{file, data, "test"};
+      builder.AddExample();
+
+      CHECK(builder.GetErrors().size() == 0);
+      CHECK(builder.GetResult()->Example.empty());
+    }
+
+    SUBCASE("Emits and error if example is not a string") {
+      auto [file, data] = CreateTOMLMock("test", R"(example = 42)");
+
+      Litr::CommandBuilder builder{file, data, "test"};
+      builder.AddExample();
+
+      CHECK(builder.GetErrors().size() == 1);
+      CHECK(builder.GetErrors()[0].Message == R"(The "example" can can only be a string.)");
+    }
+
+    SUBCASE("Extracts the example from toml data") {
+      auto [file, data] = CreateTOMLMock("test", R"(example = "Text")");
+
+      Litr::CommandBuilder builder{file, data, "test"};
+      builder.AddExample();
+
+      CHECK(builder.GetErrors().size() == 0);
+      CHECK(builder.GetResult()->Example == "Text");
+    }
+  }
 }
