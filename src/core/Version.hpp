499cc3395212ee2c6fed4ddc51a59425d7dffa81
Martin Helmut Fieber <info@martin-fieber.se>
2022-08-03 14:57:43 +0200

    Release version 1.0.0-alpha.8

--- src/core/Version.hpp
+++ src/core/Version.hpp
@@ -7,4 +7,4 @@
 // The version definition is consumed in cmake/SetVersion.cmake
 #define LITR_VERSION_MAJOR 1
 #define LITR_VERSION_MINOR 0
-#define LITR_VERSION_PATCH "0-alpha.7"
+#define LITR_VERSION_PATCH "0-alpha.8"
