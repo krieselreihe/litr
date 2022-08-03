da6739d3e827041f59858a2dd573d652b3cbd77a
Martin Helmut Fieber <info@martin-fieber.se>
2022-08-03 14:56:59 +0200

    Build for macos 10.15 comp

--- cmake/AppleBuild.cmake
+++ cmake/AppleBuild.cmake
@@ -4,5 +4,6 @@ if (CMAKE_SYSTEM_NAME STREQUAL "Darwin")
   set(CMAKE_OSX_ARCHITECTURES "arm64;x86_64" CACHE STRING "")
 
   # Support older macOS versions.
-  set(CMAKE_OSX_DEPLOYMENT_TARGET "10.9" CACHE STRING "Minimum OS X deployment version")
+  set(CMAKE_OSX_DEPLOYMENT_TARGET 10.15 CACHE STRING "Minimum OS X deployment version")
+  set(CMAKE_OSX_SYSROOT macosx10.15)
 endif ()
